b9b15f303bc8ab8bc9a543e6a441522d3d8423d3
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2016-12-01 16:36:44 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChainerCSharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
