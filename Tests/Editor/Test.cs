3c768e747eba57cf2a92e1856f3e173bd901b9f9
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2016-12-26 23:46:01 +0900

    :star: math.net.numerics

--- Tests/Editor/Test.cs
+++ Tests/Editor/Test.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
+using MathNet.Numerics.LinearAlgebra.Double;
 
-public class Test {
+public class Test
+{
+    [Test]
+    public void EditorTest()
+    {
+        Assert.IsTrue(true);
+    }
 
-	[Test]
-	public void EditorTest() {
-	    Assert.IsTrue(true);
-	}
+    [Test]
+    public void ベクトル演算()
+    {
+        var ones = new DenseVector(new double[] {1, 1, 1});
+        var twos = ones * 2;
+        Assert.AreEqual(twos[2], 2.0d);
+    }
 }
