1b3ee40933efa44db7ec0da0d236f31a62a36492
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2016-12-27 00:19:16 +0900

    :star: Forward Linear

---
+++ Tests/Editor/links/LinearTest.cs
@@ -0,0 +1,17 @@
+﻿using MathNet.Numerics.LinearAlgebra.Single;
+using NUnit.Framework;
+
+namespace chainer.links
+{
+    public class LinearTest
+    {
+        [Test]
+        public void Forwardできる()
+        {
+            var L = new Linear();
+            var x = DenseMatrix.OfArray(new float[,] {{1, 1, 1}}).Transpose();
+            var y = L.Forward(x);
+            Assert.AreEqual(y[1, 0], 2.0f);
+        }
+    }
+}
---
+++ links/Linear.cs
@@ -0,0 +1,22 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Single;
+
+namespace chainer.links
+{
+    public class Linear
+    {
+        private Matrix<float> W;
+        private Matrix<float> b;
+
+        public Linear()
+        {
+            b = DenseMatrix.OfArray(new float[,]{{1, 1, 1}}).Transpose();
+            W = DenseMatrix.OfDiagonalArray(new float[] {1, 1, 1});
+        }
+
+        public Matrix<float> Forward(Matrix<float> x)
+        {
+            return W * x + b;
+        }
+    }
+}
