3e5ce007b9f22d336e955fbf148e617494874d97
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-01-10 22:58:42 +0900

    :star: Linear function

---
+++ Tests/Editor/functions/LinearTest.cs
@@ -0,0 +1,48 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using NUnit.Framework;
+
+namespace chainer.functions
+{
+    public class LinearTest
+    {
+        [Test]
+        public void Fowardの式が正しい()
+        {
+            var x = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{1, 2, 3}}));
+            var W = new Variable(Matrix<float>.Build.DenseOfArray(new float[,]
+            {
+                {3, 4, 3},
+                {1, 2, 3},
+                {5, 4, 1},
+            }));
+            var b = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{4, 2, 1}}));
+            var testOutput = Linear.ForwardStatic(x, W, b);
+            var expected = Matrix<float>.Build.DenseOfArray(new float[,] {{24, 16, 17}}); // by chainer
+
+            chainer.Helper.MatrixAlmostEqual(testOutput.Value, expected);
+        }
+
+        [Test]
+        public void Backwardの式が正しい()
+        {
+            var x = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{1, 2, 3}}));
+            var W = new Variable(Matrix<float>.Build.DenseOfArray(new float[,]
+            {
+                {3, 4, 3},
+                {1, 2, 3},
+                {5, 4, 1},
+            }));
+            var b = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{4, 2, 1}}));
+            var target = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{0.5f, 0.5f, 0.5f}}));
+            var loss = MeanSquaredError.ForwardStatic(
+                Linear.ForwardStatic(x, W, b),
+                target
+            );
+            loss.Backward();
+            chainer.Helper.MatrixAlmostEqual(
+                x.Grad,
+                Matrix<float>.Build.DenseOfArray(new float[,] {{112.33333588f,  127.33333588f,   89f}})
+            );
+        }
+    }
+}
---
+++ functions/Linear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace chainer.functions
+{
+    public class Linear : FunctionBase<Linear>
+    {
+        protected override Variable _forward(List<Variable> inputs)
+        {
+            if (inputs.Count != 3)
+            {
+                throw new ArgumentException("function Linear requires 3 inputs");
+            }
+            var x = inputs[0].Value;
+            var W = inputs[1].Value;
+            var b = inputs[2].Value;
+
+            return new Variable(x * W.Transpose() + b);
+        }
+
+        protected override List<Matrix<float>> _backward(List<Matrix<float>> inputs, Matrix<float> gy)
+        {
+            var x = inputs[0];
+            var W = inputs[1];
+            var gx = gy * W;
+            var gW = gy.Transpose() * x;
+            var gb = Matrix<float>.Build.DenseDiagonal(1, gy.RowSums().Sum());
+            return new List<Matrix<float>>() {gx, gW, gb};
+        }
+    }
+}
