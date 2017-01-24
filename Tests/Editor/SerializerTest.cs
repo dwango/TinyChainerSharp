07fa81df242b3dc0d257e80c60307484f05ceaa4
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-01-24 22:34:42 +0900

    :star: :snake: JSON Serializer for chainer

---
+++ Tests/Editor/SerializerTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace chainer
+{
+    public class SerializerTest
+    {
+        [NUnit.Framework.Test]
+        public void シリアライズ()
+        {
+            var a = HDF5DotNet.H5.Open();
+            UnityEngine.Debug.Log(HDF5DotNet.H5.Version);
+        }
+    }
+}
