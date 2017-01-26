7c09c9f0dde8476662ec2f0bbce903ff85d0e1e2
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-01-26 23:31:49 +0900

    :star: JSON Deserializer

--- Tests/Editor/serializers/JSONSerializerTest.cs
+++ Tests/Editor/serializers/JSONSerializerTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MathNet.Numerics.LinearAlgebra;
+using NUnit.Framework;
 
 namespace chainer.serializers
 {
@@ -10,10 +11,32 @@ namespace chainer.serializers
             var link = new helper.models.VerySmallChain();
             var serializer = new JsonSerializer();
             link.Serialize(serializer);
-            UnityEngine.Debug.Log(serializer.Fetch());
-//            Assert.Contains("fc/W", serializer.Target.Keys);
-//            Assert.Contains("fc/b", serializer.Target.Keys);
+            Assert.True(serializer.Fetch().Contains("fc/W"));
         }
 
+        [Test]
+        public void JSONでのSaveLoad()
+        {
+            var link = new helper.models.VerySmallChain();
+            var serializer = new JsonSerializer();
+            link.Serialize(serializer);
+            UnityEngine.Debug.Log(serializer.Fetch());
+
+            var link2 = new helper.models.VerySmallChain();
+            link2.fc._Params["W"].Value[0, 0] = -100;
+
+            var x = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{1, 2, 3,}}));
+            Helper.AssertMatrixNotAlmostEqual(
+                link.Forward(x).Value,
+                link2.Forward(x).Value
+            );
+
+            var deserializer = new JsonDeserializer(SimpleJSON.JSON.Parse(serializer.Fetch()));
+            link2.Serialize(deserializer);
+            Helper.AssertMatrixAlmostEqual(
+                link.Forward(x).Value,
+                link2.Forward(x).Value
+            );
+        }
     }
 }
--- serializers/JSONSerializer.cs
+++ serializers/JSONSerializer.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using SimpleJSON;
+using System.Linq;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex32;
 
 namespace chainer.serializers
 {
@@ -27,12 +31,51 @@ namespace chainer.serializers
         public string Fetch()
         {
             var json = JSON.Parse("{}");
-            json["a"][-1][-1][-1] = "1";
             foreach (var kv in Target)
             {
                 json[kv.Key] = ParseVariable(kv.Value);
             }
-            return json;
+            return json.ToString();
+        }
+    }
+
+    public class JsonDeserializer : Serializer
+    {
+        private readonly JSONNode _source;
+        private readonly string _path;
+
+        public JsonDeserializer(JSONNode source, string path = "")
+        {
+            _source = source;
+            _path = path;
+        }
+
+        public override Serializer Traverse(string key)
+        {
+            return new JsonDeserializer(_source, _path + key + "/");
+        }
+
+        public override void Communicate(string key, Variable value)
+        {
+            var fullpath = _path + key;
+
+            // check variable depth because it can only handle Matrix
+            // TODO(ogaki): handle tensor whose dimention > 2
+            var matrixNode = _source[fullpath];
+            for (var currentNode = _source[fullpath]; currentNode[0].AsArray != null; currentNode = currentNode[0])
+            {
+                matrixNode = currentNode;
+            }
+
+            var matrix = new float[matrixNode.AsArray.Count, matrixNode[0].AsArray.Count];
+            for (int x = 0; x < matrix.GetLength(0); x++)
+            {
+                for (int y = 0; y < matrix.GetLength(1); y++)
+                {
+                    matrix[x, y] = matrixNode[x][y].AsFloat;
+                }
+            }
+            value.Value = Matrix<float>.Build.DenseOfArray(matrix);
         }
     }
 }
