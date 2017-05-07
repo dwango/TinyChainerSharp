14f89bf5cc7d89e1234697c109d527ec31f7a737
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-05-07 22:09:17 +0900

    :star: multiply

---
+++ Tests/Editor/functions/MultiplyTest.cs
@@ -0,0 +1,23 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using NUnit.Framework;
+
+namespace chainer.functions
+{
+    public class MultiplyTest
+    {
+        [Test]
+        public void Forwardがただしい()
+        {
+            var x = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{1, 2, 3}}));
+            var y = new Variable(Matrix<float>.Build.DenseOfArray(new float[,] {{4, -1, 0.1f}}));
+            var result = Multiply.ForwardStatic(
+                x, y
+            );
+
+            chainer.Helper.AssertMatrixAlmostEqual(
+                result.Value,
+                Matrix<float>.Build.DenseOfArray(new float[,] {{4, -2, 0.3f}})
+            );
+        }
+    }
+}
