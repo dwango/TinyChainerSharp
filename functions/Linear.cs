52e998c45f68cdff0cb3666e93673d34e3c89977
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2017-11-15 00:13:01 +0900

    :checker_flag: reduce GC for Linear.forward

--- functions/Linear.cs
+++ functions/Linear.cs
@@ -6,6 +6,9 @@ namespace chainer.functions
 {
     public class Linear : FunctionBase<Linear>
     {
+        private Matrix<float> _buffer = null;
+
+
         protected override Variable _forward(List<Variable> inputs)
         {
             if (inputs.Count != 3)
@@ -16,7 +19,19 @@ namespace chainer.functions
             var W = inputs[1].Value;
             var b = inputs[2].Value;
 
-            return new Variable(x * W.Transpose() + b);
+            Matrix<float> xw;
+            if (_buffer == null)
+            {
+                xw = x.TransposeAndMultiply(W);
+                _buffer = xw;
+            }
+            else
+            {
+                x.TransposeAndMultiply(W, _buffer);
+                xw = _buffer;
+            }
+
+            return new Variable(xw + b);
         }
 
         protected override List<Matrix<float>> _backward(List<Matrix<float>> inputs, Matrix<float> gy)
